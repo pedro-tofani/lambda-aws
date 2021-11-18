47eaa75e6e3dbd40c79300e587a4f2f2775c216c
Pedro <phtf92@gmail.com>
2021-11-18 19:20:32 -0300

    Ingestors docstring

--- de_project/ingestor.py
+++ de_project/ingestor.py
@@ -2,7 +2,19 @@ from abc import ABC, abstractmethod
 
 
 class DataIngestor(ABC):
+    """
+    [Abstract class for the differents Igestors]
+    """
+
     def __init__(self, writer, currency, api, dates_list) -> None:
+        """[Init function]
+
+        Args:
+            writer ([src.writers.DataWriter]): [Required writer]
+            currency ([str]): [The desired coin/currency]
+            api ([src.apis.ResponseApi]): [Required API]
+            dates_list ([dict]): [Dict continig the dates to be processed]
+        """
         self.writer = writer
         self.currency = currency
         self.api = api
@@ -10,6 +22,9 @@ class DataIngestor(ABC):
 
     @abstractmethod
     def ingest(self) -> None:
+        """
+        [Abstrac method to ingest the data]
+        """
         pass
 
 
